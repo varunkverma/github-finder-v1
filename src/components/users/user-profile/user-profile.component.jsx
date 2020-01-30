b44ce978710c7307ccad08d1ec7153172f9b43ac
varunkverma <varunkverma1996@gmail.com>
2020-01-30 21:47:31 +0530

    Modified UserProfile component to render user's github info

--- src/components/users/user-profile/user-profile.component.jsx
+++ src/components/users/user-profile/user-profile.component.jsx
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+import Spinner from "../../spinner/spinner.component";
 
 class UserProfile extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
   render() {
     const {
       name,
@@ -11,6 +22,7 @@ class UserProfile extends Component {
       location,
       bio,
       blog,
+      company,
       login,
       html_url,
       followers,
@@ -21,7 +33,76 @@ class UserProfile extends Component {
     } = this.props.user;
 
     const { loading } = this.props;
-    return <div>User Profile: {name}</div>;
+
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back To Search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              alt=""
+              className="round-img"
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} target="blank" className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          {public_gist && (
+            <div className="badge badge-dark">Public Gists: {public_gist}</div>
+          )}
+        </div>
+      </Fragment>
+    );
   }
 }
 export default UserProfile;
